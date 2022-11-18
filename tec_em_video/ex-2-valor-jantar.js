b160667d4fe5f361a321b9519a9474c86b457b7f
edvansabino <edvansabino@gmail.com>
2022-11-18 11:54:52 -0300

    branch correta para a playlist

--- tec_em_video/ex-2-valor-jantar.js
+++ tec_em_video/ex-2-valor-jantar.js
@@ -1,7 +1,7 @@
 // EXERCÍCIO 2 - Página 41 do Livro Lógica de Programação e Algoritmos com Javascript de Edécio Iepsen
 
 // Elaborar um programa que leia o valor de um jantar. Calcule e informe o valor da taxa do garçom (10%) e o valor total a ser pago.
-//Exemplo:
+//Exemplo-1:
 // valor do jantar: R$ 80,00
 // Taxa do garçom: R$ 8,00
 // Total a pagar: R$ 88,00
