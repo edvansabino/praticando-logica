07fce6e13fc0a26d812f016511f7d1526062e610
edvansabino <edvansabino@gmail.com>
2022-11-22 09:56:32 -0300

    Exercício HackerRank aVeryBigSum

---
+++ tec_em_video/ex-7-aVeryBigSum.js
@@ -0,0 +1,15 @@
+/*
+ * Complete the 'aVeryBigSum' function below.
+ *
+ * The function is expected to return a LONG_INTEGER.
+ * The function accepts LONG_INTEGER_ARRAY ar as parameter.
+ */
+
+function aVeryBigSum(ar) {
+  let sum = 0;
+  for (let index = 0; index < ar.length; index += 1) {
+    sum += ar[index];
+  }
+  return sum;
+}
+console.log(aVeryBigSum([1000000001, 1000000002, 1000000003, 1000000004, 1000000005]));
