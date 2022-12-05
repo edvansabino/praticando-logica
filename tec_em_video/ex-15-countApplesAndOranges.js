1fa6f805ff8a4eff25e053eb50ddf9ecb044c54a
edvansabino <edvansabino@gmail.com>
2022-12-05 16:53:00 -0300

    Exercício 15 count apples and oranges

---
+++ tec_em_video/ex-15-countApplesAndOranges.js
@@ -0,0 +1,20 @@
+function countApplesAndOranges(s, t, a, b, apples, oranges) {
+  let applesCount = 0;
+  let orangesCount = 0;
+
+  apples.forEach((apple) => {
+    if (apple + a >= s && apple + a <= t) {
+      applesCount += 1;
+    }
+  });
+
+  oranges.forEach((orange) => {
+    if (orange + b <= t && orange + b >= s) {
+      orangesCount += 1;
+    }
+  });
+  console.log(applesCount);
+  console.log(orangesCount);
+}
+
+countApplesAndOranges(7, 10, 4, 12, [2, 3, -4], [3, -2, -4]);
